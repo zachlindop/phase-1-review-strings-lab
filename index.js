6052f07cd7047052cef9e196f56a9703379fc170
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-07-19 09:13:47 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+let currentUser = "hello world!"
+let welcomeMessage = `Welcome to Flatbook, Zach, ${currentUser}`
+const upperCaseCurrentUser = currentUser.toUpperCase();
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}`
+let shortGreeting = "Welcome, "
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+shortGreeting = `Welcome, ${firstInitial}!`
